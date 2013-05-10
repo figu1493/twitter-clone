c4ba117d2940821faebbd8316727da85a049b957
John Figueiredo <jfigueir33@gmail.com>
2013-05-09 21:07:05 -0700

    Finish Layout and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 TwitterClone::Application.routes.draw do
-get   "static_pages/home"
-get   "static_pages/help"
-get   "static_pages/about"
-get   "static_pages/contact"
+  get "users/new"
 
-get   "microposts/index"
-get   "microposts/edit"
-post  "microposts/new"
-get   "microposts/show"
+root to: 'static_pages#home'
+  
+match '/help',    to: 'static_pages#help'
+match '/about',   to: 'static_pages#about'
+match '/contact', to: 'static_pages#contact'
 
-get   "users/index"
-get   "users/edit"
-post  "users/new"
-get   "users/show"
+match '/signup',  to: 'users#new'
 
   resources :microposts
   resources :users
