c7d2df6649c74d7a0077fa9d2c8847861a70dc34
John Figueiredo <jfigueir33@gmail.com>
2013-05-15 19:45:39 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by_id(params[:id])
   end
 
   def new
+  	@user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+    	flash[:success] = "Welcome to the Twitter Clone!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "John Figueiredo"
+    email    "john@johnfigueiredo.com"
+    password "jfig3333"
+    password_confirmation "jfig3333"
+  end
+end
